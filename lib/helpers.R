50d2d54f014c6287752cbf1232b4eb6d8ae5b83c
Richard Lucas <rlucas11@yahoo.com>
2013-07-03 11:49:48 -0400

    getVariables function and missing values

--- lib/helpers.R
+++ lib/helpers.R
@@ -27,23 +27,29 @@ getMasterVariable <- function(variableName, masterFileName=masterFile) {
     return(master[,c(idName, variableName)])
 }
 
-getOneVariable <- function(variableStem, dataFileName=originalDataFile, masterFileName=masterFile,
-                        wide=FALSE, waves=waveLabels) {
-    master <- read.dta(paste(pathOriginalData, fileName, sep = ""))
+
+getVariables <- function(variables, dataFileName=originalDataFile,
+                         masterFileName=masterFile, wide=FALSE,
+                         waves=waveLabels) {
+    master <- read.dta(paste(pathOriginalData, masterFileName, sep = ""))
     master <- data.frame(master[,c(idName)], stringsAsFactors=FALSE)
     names(master) <- idName
     for (i in waves) {
-        newDataFileName <- paste(pathOriginalData, sub(charsToSub, i, dataFileName), sep = "")
-        newVariableName <- paste(i, variableStem, sep = "")
+        newDataFileName <- paste(pathOriginalData, sub(charsToSub, i, dataFileName),
+                                 sep = "")
+        newVariableNames <- paste(i, variables, sep = "")
         data <- read.dta(newDataFileName)
-        data <- data[,c(idName, newVariableName)]
+        data <- data[,c(idName, newVariableNames)]
         master <- merge(master, data, by = idName, all.x=TRUE)
     }
     if (wide==FALSE) {
         master <- melt(master, id.vars=idName)
-        master$variable <- str_replace(master$variable, variableStem, "")
-        names(master)[2:3] <- c("wave",variableStem)
+        master$wave <- substring(master$variable,1,1)
+        master$variable <- substring(master$variable,2)
+        form <- as.formula(paste(idName, " + wave ~ variable", sep=""))
+        master <- dcast(master, form, value.var="value")
     }
     return(master)
 }
-        
+
+iRecode <- function (x, rstring) { recode (x, rstring) }
--- munge/01-A.R
+++ munge/01-A.R
@@ -1,5 +1,13 @@
 # Example preprocessing script.
 
 sex <- getMasterVariable("sex")
-lifeSat <- getVariable("losat")
-partnerNumbers <- getVariable("hhpxid")
+cache('sex')
+
+satVars <- paste("losat",c("","eo","fs","ft","hl","lc","nl","sf","yh"), sep="")
+satData <- getVariables(satVars)
+recodeString <- "-10:-1=NA"
+satData[, satVars] <- lapply (satData[, satVars], iRecode, recodeString)
+cache('satData')
+
+partnerNumbers <- getVariables("hhpxid")
+cache('partnerNumbers')
